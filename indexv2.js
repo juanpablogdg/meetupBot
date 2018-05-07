6520af9c743965fa9c99ca69a88641793fdf7004
jpgonzalez <jpgonzalez@paradigmadigital.com>
2018-05-07 19:23:36 +0200

    Refactorizacion de código

--- indexv2.js
+++ indexv2.js
@@ -1,16 +1,19 @@
 const request = require('request');
-const _ = require('lodash');
 const dateFormat = require('dateformat');
 const runtimeConfig = require('cloud-functions-runtime-config');
 
+const MAX_DATE='12/31/2040';
 
 
-let assistantResponse = {
-    fulfillmentText: ''
-   // text: ''
-};
-
-
+/**
+ * Retrieves the parameters from the request argument
+ *
+ * @param request
+ * @returns {any}
+ */
+function getParameters(request){
+    return req.body.queryResult.parameters;
+}
 /**
  * Evaluate if value is undefined
  * @param value value to check
@@ -34,18 +37,59 @@ function getTopic(parameters) {
     }
 }
 
+/**
+ * Retrieves the startDate from the date-period parameter
+ * @param parameters
+ * @returns {Date}
+ */
 function getDateFrom(parameters) {
     return new Date(parameters['date-period'].startDate);
 }
 
 /**
- *
+ * Retrieves the endDate from the date-period parameter
  * @param parameters
  * @returns {Date}
  */
 function getDateTo(parameters) {
     return new Date(parameters['date-period'].endDate);
 }
+
+/**
+ * Function to order two dates
+ * @param dateA
+ * @param dateB
+ * @returns {number}
+ */
+function orderDateAsc(dateA,dateB){
+    return dateA.eventDate.getTime() - dateB.eventDate.getTime();
+}
+
+/**
+ * This functions checks if the meetup has an event
+ * @param meetup
+ * @returns {boolean}
+ */
+function meetupsWithEventAvailable(meetup){
+    return !isUndefined(meetup.next_event);
+}
+
+
+/**
+ * This functions transforms a meetup with the meetup.com API format to our own detailed format
+ * @param meetup API format
+ * @returns {{name: *, link: *, eventName: string, eventDate: Date}}
+ */
+function meetupAPItoMeetupDetail(meetup){
+    let detail = {
+        name: meetup.name,
+        link: meetup.link,
+        eventName: isUndefined(meetup.next_event.name) ? 'error' : meetup.next_event.name,
+        eventDate: new Date(meetup.next_event.time)
+    };
+    return detail;
+}
+
 
 /**
  * Responds to any HTTP request that can provide a "message" field in the body.
@@ -55,25 +99,32 @@ function getDateTo(parameters) {
  */
 exports.meetup = (req, res) => {
 
-    const parameters = req.body.queryResult.parameters;
 
-    console.log('Request ' + JSON.stringify(req.body));
+    //Response
+    let assistantResponse = {
+        fulfillmentText: ''
+    };
+
+
+    const parameters = getParameters(req);
+
+   /* console.log('Request ' + JSON.stringify(req.body));
 
 
     console.log("Parameters " + JSON.stringify(parameters));
     console.log("parameters " + parameters['date-period']);
-
+*/
     topic = getTopic(parameters);
     dateFrom = (parameters['date-period'] === '') ? new Date() : getDateFrom(parameters);
-    dateTo = (parameters['date-period'] === '') ? new Date('12/31/2040') : getDateTo(parameters);
+    dateTo = (parameters['date-period'] === '') ? new Date(MAX_DATE) : getDateTo(parameters);
 
 
-    const topicParam = topic === '' ? '' : '&text=' + topic;
+    const topicParam = (topic === '') ? '' : '&text=' + topic;
 
 
 
 
-    runtimeConfig.getVariable('dev-config', 'api-key').then((apiKey)=>{
+    runtimeConfig.getVariable('dev-config', 'api-key').then((apiKey) => {
 
         const API_URL = 'https://api.meetup.com/';
         const FIND_GROUPS = 'find/groups?';
@@ -97,7 +148,7 @@ exports.meetup = (req, res) => {
 
         console.log(JSON.stringify(meetups));
 
-        responseJson = meetups.filter(function (meetup) {
+        /*responseJson = meetups.filter(function (meetup) {
             return !isUndefined(meetup.next_event);
         })
             .filter(function (meetup) {
@@ -111,16 +162,25 @@ exports.meetup = (req, res) => {
                     eventDate: new Date(meetup.next_event.time)
                 };
                 return detail;
-            });
-        //Date order array
-        responseJson.sort(function (a, b) {
+            });*/
+
+        //Date ordered array
+      /*  responseJson.sort(function (a, b) {
             return a.eventDate.getTime() - b.eventDate.getTime()
-        });
+        });*/
+
+      responseJson.filter(meetupsWithEventAvailable)
+                  .filter(function (meetup) {
+                            return meetup.next_event.time > dateFrom.getTime() && meetup.next_event.time < dateTo.getTime();
+                        })
+                .map(meetupAPItoMeetupDetail);
 
 
-        let responseToUser = humanizeResponse(req, responseJson);
-        assistantResponse.fulfillmentText = responseToUser;
-      //  assistantResponse.text = responseToUser;
+
+       responseJson.sort(orderDateAsc);
+
+
+        assistantResponse.fulfillmentText =  humanizeResponse(req, responseJson);
 
         res.status(200).send(assistantResponse);
 
@@ -141,7 +201,7 @@ exports.meetup = (req, res) => {
 
     function humanizeResponse(req, responseJson) {
 
-        const parameters = req.body.queryResult.parameters;
+        const parameters =  getParameters(req);
         let requestSource = (req.body.originalDetectIntentRequest) ? req.body.originalDetectIntentRequest.source : undefined;
 
 
@@ -150,6 +210,7 @@ exports.meetup = (req, res) => {
 
         topic = getTopic(parameters);
 
+        //Header info
         if (topic !== '') {
             extraInfo += ' sobre ' + topic;
         }
@@ -158,30 +219,25 @@ exports.meetup = (req, res) => {
             extraInfo += ' entre ' + dateFormat(getDateFrom(parameters), 'dd/mm/yy') + ' y ' + dateFormat(getDateTo(parameters), 'dd/mm/yy');
         }
 
+        //Detail info
         if (responseJson.length > 0) {
 
             responseText = 'He encontrado ' + responseJson.length + ' resultados ' + extraInfo + '. Son los siguientes :\n';
 
 
+            //Tendremos 2 respuestas. Una para google assistant, preparada para ser leída y otra para slack, preparada para hacer click.
             responseJson.forEach(function (detail) {
                 if (requestSource === 'google') {
-
-
-                    let detailAssistanMessage = 'El grupo ' + detail.name + ' organiza ' + detail.eventName + ' el próximo día ' + dateFormat(detail.eventDate, 'dd/mm/yy') + '.\n';
-                    responseText = responseText.concat(detailAssistanMessage);
-
+                    responseText = responseText.concat( 'El grupo ' + detail.name + ' organiza ' + detail.eventName + ' el próximo día ' + dateFormat(detail.eventDate, 'dd/mm/yy') + '.\n');
                 }
                 else {
-                    let detailSlackMessage = '<' + detail.link + ' | ' + detail.name + '> - ' +
-                        '*' + detail.eventName + '* el próximo día ' + dateFormat(detail.eventDate, 'dd/mm/yy') + '\n';
-                    responseText = responseText.concat(detailSlackMessage);
+                    responseText = responseText.concat('<' + detail.link + ' | ' + detail.name + '> - ' +
+                        '*' + detail.eventName + '* el próximo día ' + dateFormat(detail.eventDate, 'dd/mm/yy') + '\n');
                 }
-
-
             });
 
 
-        } else {
+        } else { //Data not found
 
             responseText = 'Lo siento no he podido encontrar nada' + extraInfo;
         }
